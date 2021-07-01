7951baa9c8a180ee33de80e15117388cbf22a598
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2021-07-01 22:26:25 +0900

    add player to hero section and fontawesome icons

--- src/components/HeroSection/HeroItem.js
+++ src/components/HeroSection/HeroItem.js
@@ -1,4 +1,10 @@
 import React, { Fragment } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { faExpand } from "@fortawesome/free-solid-svg-icons";
+
 import classes from "./HeroItem.module.css";
 import kryptonite from "../../assets/images/kryptonite.jpeg";
 
@@ -10,7 +16,21 @@ const HeroItem = () => {
           <div className={classes.playerPreview}>
             <img src={kryptonite} alt="hero" />
           </div>
-          <div className={classes.playerControl}></div>
+          <div className={classes.playerControl}>
+            <div className={classes.playerBtn}>
+              <FontAwesomeIcon className={classes.icon} icon={faPlay} />
+            </div>
+            <div className={classes.line}>
+              <div className={classes.progress}></div>
+            </div>
+            <div className={classes.time}>1:25</div>
+            <div className={classes.playerBtn}>
+              <FontAwesomeIcon className={classes.icon} icon={faVolumeUp} />
+            </div>
+            <div className={classes.expand}>
+              <FontAwesomeIcon className={classes.icon} icon={faExpand} />
+            </div>
+          </div>
         </div>
         <div className="mainDetails">
           <h2>Hello World</h2>
