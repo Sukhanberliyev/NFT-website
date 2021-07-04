d9c4108a0848519c83f617edbef35d0907ee5292
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2021-07-05 00:10:05 +0900

    add timer to hero-details

--- src/components/HeroSection/HeroDetails.js
+++ src/components/HeroSection/HeroDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./HeroDetails.module.css";
 import MainButton from "../UI/MainButton";
@@ -7,9 +7,28 @@ import OutlineButton from "../UI/OutlineButton";
 import avatar from "../../assets/avatars/elize.png";
 import eth from "../../assets/avatars/eth.png";
 
-
-
 const HeroDetails = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    if (hours > 0) {
+      setTimeout(() => setMinutes(hours - 1), 600000);
+    } else {
+      setHours(7 - 1);
+    }
+    if (minutes > 0) {
+      setTimeout(() => setMinutes(minutes - 1), 60000);
+    } else {
+      setMinutes(60 - 1);
+    }
+    if (seconds > 0) {
+      setTimeout(() => setSeconds(seconds - 1), 1000);
+    } else {
+      setSeconds(60 - 1);
+    }
+  }, [seconds, minutes, hours]);
   return (
     <div className={classes.mainDetails}>
       <h1>the creator network®</h1>
@@ -36,15 +55,15 @@ const HeroDetails = () => {
         <p className={classes.mainInfo}>Auction end in</p>
         <ul className={classes.mainTimer}>
           <li className={classes.mainBox}>
-            <p className={classes.mainNumber}>07</p>
+            <p className={classes.mainNumber}>0{hours}</p>
             <p className={classes.mainTime}>hrs</p>
           </li>
           <li className={classes.mainBox}>
-            <p className={classes.mainNumber}>48</p>
+            <p className={classes.mainNumber}>{minutes}</p>
             <p className={classes.mainTime}>mins</p>
           </li>
           <li className={classes.mainBox}>
-            <p className={classes.mainNumber}>59</p>
+            <p className={classes.mainNumber}>{seconds}</p>
             <p className={classes.mainTime}>secs</p>
           </li>
         </ul>
