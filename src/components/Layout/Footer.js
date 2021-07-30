c6d3b7ee35c957e72a4714a292e3b97a83fabb6e
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2021-07-30 14:09:02 +0900

    add style to footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// importing css
+import "./Footer.css"
 // importing components
 import Container from "./Container";
 
 const Footer = () => {
   return (
-    <div>
+    <footer>
       <Container>
-        <div className="footerRaw">
+        <div className="footerRow">
           <div className="footerColumn">
             <Link className="footerLogo">
               <img src="" alt="logo" />
             </Link>
-            <p>The New Creative Economy</p>
+            <p className="footerInfo">The New Creative Economy</p>
             <div className="footerMode">
               <p className="footerDetails">Dark mode</p>
               {/* toggleHandler goes here */}
@@ -23,24 +25,24 @@ const Footer = () => {
             <div className="footerGroup">
               <div className="footerHead">
                 Cryptonite
-                <svg></svg>
+                {/* <svg></svg> */}
               </div>
               <div className="footerBody">
                 <div className="footerMenu">
-                  <Link>Discover</Link>
-                  <Link>Connect Wallet</Link>
+                  <Link className="footerLink">Discover</Link>
+                  <Link className="footerLink">Connect Wallet</Link>
                 </div>
               </div>
             </div>
             <div className="footerGroup">
               <div className="footerHead">
                 Info
-                <svg></svg>
+                {/* <svg></svg> */}
               </div>
               <div className="footerBody">
                 <div className="footerMenu">
-                  <Link>FAQ</Link>
-                  <Link>Create item</Link>
+                  <Link className="footerLink">FAQ</Link>
+                  <Link className="footerLink">Create item</Link>
                 </div>
               </div>
             </div>
@@ -51,14 +53,15 @@ const Footer = () => {
             <form className="subscription" action="">
               <input className="subscriptionInput" type="email" name="email" placeholder="Enter your email" required="required" />
               <button className="subscriptionBtn">
-                <svg></svg>
+                {/* <svg></svg> */}
+                N
               </button>
             </form>
           </div>
         </div>
         <div className="footerFoot"></div>
       </Container>
-    </div>
+    </footer>
   );
 };
 
