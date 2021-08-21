614345afac09f7ea4533ab95d0c239eb08f7c085
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2021-08-21 23:35:03 +0900

    add main tab logic to FAQ.js

--- src/components/HowItWorks/FAQ.js
+++ src/components/HowItWorks/FAQ.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
@@ -11,6 +10,12 @@ import "./FAQ.css";
 const faqOptions = ["General", "Support", "Hosting", "Product"];
 
 const FAQ = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <section className="faq">
       <Container>
@@ -28,17 +33,99 @@ const FAQ = () => {
         <div className="faqRow">
           <div className="faqColumn">
             <div className="faqNav">
-              <Link className="faqLink">General</Link>
-              <Link className="faqLink">Support</Link>
-              <Link className="faqLink">Hosting</Link>
-              <Link className="faqLink">Product</Link>
+              <div
+                className={toggleState === 1 ? "faqLink active" : "faqLink"}
+                onClick={() => toggleTab(1)}
+              >
+                General
+              </div>
+              <div
+                className={toggleState === 2 ? "faqLink active" : "faqLink"}
+                onClick={() => toggleTab(2)}
+              >
+                Support
+              </div>
+              <div
+                className={toggleState === 3 ? "faqLink active" : "faqLink"}
+                onClick={() => toggleTab(3)}
+              >
+                Hosting
+              </div>
+              <div
+                className={toggleState === 4 ? "faqLink active" : "faqLink"}
+                onClick={() => toggleTab(4)}
+              >
+                Product
+              </div>
             </div>
           </div>
           <div className="faqColumn">
-            <div className="faqBox">
+            {/* *************************** FIRST FAQ START *************************** */}
+            <div className={toggleState === 1 ? "faqBox active" : "faqBox"}
+                onClick={() => toggleTab(1)}>
               <div className="faqItem">
                 <div className="faqHead">
-                  How does it works
+                  This is for first faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for first faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for first faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for first faq
                   <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
                 </div>
                 <div className="faqBody">
@@ -57,6 +144,263 @@ const FAQ = () => {
                 </div>
               </div>
             </div>
+            {/* *************************** FIRST FAQ END *************************** */}
+
+            {/* *************************** SECOND FAQ START *************************** */}
+            <div className={toggleState === 2 ? "faqBox active" : "faqBox"}
+                onClick={() => toggleTab(2)}>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for second faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for second faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for second faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for second faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* *************************** SECOND FAQ END *************************** */}
+            {/* *************************** THIRD FAQ START *************************** */}
+            <div className={toggleState === 3 ? "faqBox active" : "faqBox"}
+                onClick={() => toggleTab(3)}>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for third faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for third faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for third faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for third faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* *************************** THIRD FAQ END *************************** */}
+            {/* *************************** FOURTH FAQ START *************************** */}
+            <div className={toggleState === 4 ? "faqBox active" : "faqBox"}
+                onClick={() => toggleTab(4)}>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for fourth faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for fourth faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for fourth faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+              <div className="faqItem">
+                <div className="faqHead">
+                  This is for fourth faq
+                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                </div>
+                <div className="faqBody">
+                  <div className="faqContent">
+                    The Stacks series of products: Stacks: Landing Page Kit,
+                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
+                    production-ready library of stackable content blocks built
+                    in React Native. Mix-and-match dozens of responsive elements
+                    to quickly configure your favorite landing page layouts or
+                    hit the ground running with 10 pre-built templates, all in
+                    light or dark mode."
+                  </div>
+                  <div className="faqBtn">
+                    <OutlineButton>Learn more</OutlineButton>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* *************************** FOURTH FAQ END *************************** */}
           </div>
         </div>
       </Container>
