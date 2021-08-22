56cb6967ef2c1de1f7ba637a088635e95fbfe700
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2021-08-22 18:19:45 +0900

    Add accordion logic to FAQ.js

--- src/components/HowItWorks/FAQ.js
+++ src/components/HowItWorks/FAQ.js
@@ -11,6 +11,11 @@ const faqOptions = ["General", "Support", "Hosting", "Product"];
 
 const FAQ = () => {
   const [toggleState, setToggleState] = useState(1);
+  const [open, setOpen] = useState(false);
+
+  const dropHandler = () => {
+    setOpen(!open);
+  };
 
   const toggleTab = (index) => {
     setToggleState(index);
@@ -61,22 +66,27 @@ const FAQ = () => {
           </div>
           <div className="faqColumn">
             {/* *************************** FIRST FAQ START *************************** */}
-            <div className={toggleState === 1 ? "faqBox active" : "faqBox"}
-                onClick={() => toggleTab(1)}>
+            <div
+              className={toggleState === 1 ? "faqBox active" : "faqBox"}
+              onClick={() => toggleTab(1)}
+            >
               <div className="faqItem">
-                <div className="faqHead">
-                  This is for first faq
-                  <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
+                <div className="faqHead" onClick={dropHandler}>
+                  NFT? ERC-721 tokens?
+                  <FontAwesomeIcon
+                    className={open ? "faqIcon" : "faqIcon active"}
+                    icon={faChevronDown}
+                  />
                 </div>
-                <div className="faqBody">
+                <div className={`faqBody ${open ? "displayBlock" : ""}`}>
                   <div className="faqContent">
-                    The Stacks series of products: Stacks: Landing Page Kit,
-                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
-                    production-ready library of stackable content blocks built
-                    in React Native. Mix-and-match dozens of responsive elements
-                    to quickly configure your favorite landing page layouts or
-                    hit the ground running with 10 pre-built templates, all in
-                    light or dark mode."
+                    NFT stands for non-fungible tokens like ERC-721 (a smart
+                    contract standard) tokens which are hosted on Ethereum’s own
+                    blockchain. NFTs are unique digital items such as
+                    collectibles or artworks or game items. As an artist, by
+                    tokenizing your work you both ensure that it is unique and
+                    brand it as your work. The actual ownership is
+                    blockchain-managed.
                   </div>
                   <div className="faqBtn">
                     <OutlineButton>Learn more</OutlineButton>
@@ -85,18 +95,16 @@ const FAQ = () => {
               </div>
               <div className="faqItem">
                 <div className="faqHead">
-                  This is for first faq
+                  What is a crypto wallet?
                   <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
                 </div>
                 <div className="faqBody">
                   <div className="faqContent">
-                    The Stacks series of products: Stacks: Landing Page Kit,
-                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
-                    production-ready library of stackable content blocks built
-                    in React Native. Mix-and-match dozens of responsive elements
-                    to quickly configure your favorite landing page layouts or
-                    hit the ground running with 10 pre-built templates, all in
-                    light or dark mode."
+                    A crypto wallet is an application or hardware device that
+                    allows individuals to store and transfer digital assets like
+                    cryptocurrencies and Non-Fungible Tokens (NFTs). This guide
+                    breaks down the crypto wallet basics that you'll need to
+                    know before creating and trading items on Cryptonite.
                   </div>
                   <div className="faqBtn">
                     <OutlineButton>Learn more</OutlineButton>
@@ -105,18 +113,15 @@ const FAQ = () => {
               </div>
               <div className="faqItem">
                 <div className="faqHead">
-                  This is for first faq
+                  What crypto wallets can I use with Cryptonite?
                   <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
                 </div>
                 <div className="faqBody">
                   <div className="faqContent">
-                    The Stacks series of products: Stacks: Landing Page Kit,
-                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
-                    production-ready library of stackable content blocks built
-                    in React Native. Mix-and-match dozens of responsive elements
-                    to quickly configure your favorite landing page layouts or
-                    hit the ground running with 10 pre-built templates, all in
-                    light or dark mode."
+                    Just as there are many banks and credit cards, there are
+                    many different crypto wallet providers to choose from. They
+                    all serve the same purpose, but each one takes a different
+                    approach and makes different tradeoffs.
                   </div>
                   <div className="faqBtn">
                     <OutlineButton>Learn more</OutlineButton>
@@ -125,18 +130,15 @@ const FAQ = () => {
               </div>
               <div className="faqItem">
                 <div className="faqHead">
-                  This is for first faq
+                  How do I purchase Ethereum (ETH)?
                   <FontAwesomeIcon className="faqIcon" icon={faChevronDown} />
                 </div>
                 <div className="faqBody">
                   <div className="faqContent">
-                    The Stacks series of products: Stacks: Landing Page Kit,
-                    Stacks: Portfolio Kit, Stacks: eCommerce Kit. "Stacks is a
-                    production-ready library of stackable content blocks built
-                    in React Native. Mix-and-match dozens of responsive elements
-                    to quickly configure your favorite landing page layouts or
-                    hit the ground running with 10 pre-built templates, all in
-                    light or dark mode."
+                    Ether is the native currency of the Ethereum network and
+                    it’s commonly abbreviated to ETH, which is its ticker
+                    symbol. You need ETH to pay for some of your interactions
+                    with the blockchain and to pay for the items you buy.
                   </div>
                   <div className="faqBtn">
                     <OutlineButton>Learn more</OutlineButton>
@@ -147,8 +149,10 @@ const FAQ = () => {
             {/* *************************** FIRST FAQ END *************************** */}
 
             {/* *************************** SECOND FAQ START *************************** */}
-            <div className={toggleState === 2 ? "faqBox active" : "faqBox"}
-                onClick={() => toggleTab(2)}>
+            <div
+              className={toggleState === 2 ? "faqBox active" : "faqBox"}
+              onClick={() => toggleTab(2)}
+            >
               <div className="faqItem">
                 <div className="faqHead">
                   This is for second faq
@@ -232,8 +236,10 @@ const FAQ = () => {
             </div>
             {/* *************************** SECOND FAQ END *************************** */}
             {/* *************************** THIRD FAQ START *************************** */}
-            <div className={toggleState === 3 ? "faqBox active" : "faqBox"}
-                onClick={() => toggleTab(3)}>
+            <div
+              className={toggleState === 3 ? "faqBox active" : "faqBox"}
+              onClick={() => toggleTab(3)}
+            >
               <div className="faqItem">
                 <div className="faqHead">
                   This is for third faq
@@ -317,8 +323,10 @@ const FAQ = () => {
             </div>
             {/* *************************** THIRD FAQ END *************************** */}
             {/* *************************** FOURTH FAQ START *************************** */}
-            <div className={toggleState === 4 ? "faqBox active" : "faqBox"}
-                onClick={() => toggleTab(4)}>
+            <div
+              className={toggleState === 4 ? "faqBox active" : "faqBox"}
+              onClick={() => toggleTab(4)}
+            >
               <div className="faqItem">
                 <div className="faqHead">
                   This is for fourth faq
