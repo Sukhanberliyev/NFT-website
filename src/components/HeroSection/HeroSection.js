d0e8e53e51b6eec9916353fbb67d8c75c8ae61b6
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2021-10-18 14:02:17 +0900

    Add typed.js to title in HeroSection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -8,8 +8,38 @@ import "./ReactSlick.css";
 import Container from "../Layout/Container";
 import OutlineButton from "../UI/OutlineButton";
 import HeroItem from "./HeroItem";
+import Typed from "typed.js";
 
 const HeroSection = (props) => {
+  // Create reference to store the DOM element containing the animation
+  const el = useRef(null);
+  // Create reference to store the Typed instance itself
+  const typed = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        "<span>The new creative</span> <span>economy</span>",
+        "<span>Create your dream</span> <span>gallery</span>",
+        "<span>Discover, and</span> <span>collect NFTs</span>",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: "|",
+      autoInsertCss: true,
+    };
+
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    };
+  }, []);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -25,7 +55,7 @@ const HeroSection = (props) => {
         <main className={classes.main}>
           <div className={classes.mainHead}>
             <p>CREATE, EXPLORE, & COLLECT DIGITAL ART NFTs.</p>
-            <h3>The new creative economy</h3>
+            <h3 style={{ whiteSpace: "pre" }} ref={el} />
             <div className={classes.heroButton}>
               <OutlineButton>Start your search</OutlineButton>
             </div>
