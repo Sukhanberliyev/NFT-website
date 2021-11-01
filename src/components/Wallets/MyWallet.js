c50ee0b7a3d5120cd657a8eae970bf2a48163d46
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2021-11-01 09:12:36 +0900

    Add ShowMoreHandler functions to wallet section

--- src/components/Wallets/MyWallet.js
+++ src/components/Wallets/MyWallet.js
@@ -1,48 +1,56 @@
-import React from 'react';
-import classes from './MyWallet.module.css';
+import React, { useState } from "react";
+import classes from "./MyWallet.module.css";
 
 const MyWallet = () => {
-	return (
-		<div className={classes.myWallet}>
-			<div className={classes.walletContainer}>
-				<div className={classes.mainImg}>
-					<img src='/assets/images/ethereum.svg' alt='ethereum' />
-				</div>
-				<h2 className={classes.mainText}>Sign in with your wallet</h2>
-				<p className={classes.subText}>
-					Sign in with one of available wallet providers or create a new wallet.{' '}
-					<span>What is a wallet?</span>
-				</p>
-				<ul>
-					<li className={classes.walletList}>
-						<div className={classes.walletImg}>
-							<img src='/assets/images/metamask.png' alt='' />
-						</div>
-						<p>Metamask</p>
-					</li>
-					<li className={classes.walletList}>
-						<div className={classes.walletImg}>
-							<img src='/assets/images/coinbaseWallet.png' alt='' />
-						</div>
-						<p>Coinbase</p>
-					</li>
-					<li className={classes.walletList}>
-						<p>Show more options</p>
-					</li>
-				</ul>
-				<ul>
-					<li className={classes.walletList}>
-						<img src='' alt='' />
-						<p>Coinbase</p>
-					</li>
-					<li className={classes.walletList}>
-						<img src='' alt='' />
-						<p>Formatic</p>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+  const [open, setOpen] = useState(false);
+
+  const showMoreHandler = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className={classes.myWallet}>
+      <div className={classes.walletContainer}>
+        <div className={classes.mainImg}>
+          <img src="/assets/images/ethereum.svg" alt="ethereum" />
+        </div>
+        <h2 className={classes.mainText}>Sign in with your wallet</h2>
+        <p className={classes.subText}>
+          Sign in with one of available wallet providers or create a new wallet.{" "}
+          <span>What is a wallet?</span>
+        </p>
+        <ul>
+          <li className={classes.walletList}>
+            <div className={classes.walletImg}>
+              <img src="/assets/images/metamask.png" alt="" />
+            </div>
+            <p>Metamask</p>
+          </li>
+          <li className={classes.walletList}>
+            <div className={classes.walletImg}>
+              <img src="/assets/images/coinbaseWallet.png" alt="" />
+            </div>
+            <p>Coinbase</p>
+          </li>
+          <li className={classes.walletList} onClick={showMoreHandler}>
+            {!open ? <p>Show more options</p> : <p>Show less options</p>}
+          </li>
+        </ul>
+        {open && (
+          <ul>
+            <li className={classes.walletList}>
+              <img src="" alt="" />
+              <p>Coinbase</p>
+            </li>
+            <li className={classes.walletList}>
+              <img src="" alt="" />
+              <p>Formatic</p>
+            </li>
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MyWallet;
